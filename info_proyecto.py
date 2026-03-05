108c43efe30ee1657703f67364129fe09a5c8ebc
maxrivasl <maxrivas@uees.edu.ec>
2026-03-05 19:53:55 +0000

    Archivo funcion descripcion_proyecto

--- info_proyecto.py
+++ info_proyecto.py
@@ -2,4 +2,4 @@ def descripcion_proyecto():
     titulo =  "Restaurante con Gestión de Pedidos"
     descripcion = "Aplicación que representa el flujo de pedidos de una cocina, coordinando la preparación de platos según los recursos e ingredientes disponibles."
     print("Título: ", titulo)
-    print("Descripción: ", descripcion) 
+    print("Descripción: ", descripcion)
